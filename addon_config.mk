3259f069a5420590221343fb2d6d62eebf0054e3
Christopher Baker <me@christopherbaker.net>
2015-11-16 00:09:34 -0600

    Add addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxSSLManager
+	ADDON_DESCRIPTION = A system for managing SSL certificates.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "ssl" "openssl"
+	ADDON_URL = http://github.com/bakercp/ofxSSLManager
